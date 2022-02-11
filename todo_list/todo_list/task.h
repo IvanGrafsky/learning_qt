f743d3211f70b9c5fcb80738e76150e8da309c47
Ivan Grafskiy <igrafskiy@gmail.com>
2022-02-11 21:06:27 +0300

    Todo list application

---
+++ todo_list/todo_list/task.h
@@ -0,0 +1,39 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <QWidget>
+#include <QString>
+
+namespace Ui {
+class Task;
+}
+
+class Task : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Task(const QString& name, QWidget *parent = nullptr);
+    ~Task();
+
+    void setName(const QString& name);
+    QString name() const;
+    bool isCompleted() const;
+
+public slots:
+    void rename();
+
+signals:
+    void removed(Task* task);
+    void statusChanged(Task* task);
+
+
+private:
+    Ui::Task *ui;
+
+private slots:
+    void checked(bool checked);
+
+};
+
+#endif // TASK_H
